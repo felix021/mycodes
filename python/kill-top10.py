83734dcaccdb7dbf7345b8a02a2c2e76abbba645
FengMin.Felix <fengmin.felix@bytedance.com>
2020-11-27 15:51:42 +0800

    kill top10: bash -> python, for better filter support
    Change-Id: I6baf92edb26f565e71aa9307c228d571f3d26c87

---
+++ python/kill-top10.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#coding: utf-8
+
+import os
+import time
+import subprocess
+
+ps = subprocess.check_output(["ps", "-axo", "pid,rss,user,cmd", "-k-rss", "--no-headers"])
+
+kill_list = []
+
+for line in ps.split("\n"):
+    pid, rss, user, cmd = line.strip().split(" ", 3)
+
+    if user in ["root"]:
+        continue
+
+    if rss <= 2 * 1024 * 1024: # 2GB
+        continue
+
+    kill_list.append([pid, rss, user, cmd])
+    if len(kill_list) == 10:
+        break
+
+kill_log = open("/tmp/killed-%s.txt" % time.strftime("%Y%m%d"), "w")
+print >>kill_log, ["pid", "rss", "user", "cmd"]
+for row in kill_list:
+    print >>kill_log, row
+    cmd = "kill -int %s" % (row[0])
+    os.system(cmd)
