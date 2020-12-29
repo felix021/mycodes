43d6d15be27d07f7e870f9fa52fb760bbcff8585
FengMin.Felix <fengmin.felix@bytedance.com>
2020-12-29 11:12:20 +0800

    update kill top10
    Change-Id: I918a19925f5e7ecf3862a2d833335ee40ed82b32

--- python/kill-top10.py
+++ python/kill-top10.py
@@ -2,14 +2,27 @@
 #coding: utf-8
 
 import os
+import sys
 import time
 import subprocess
+
+kill_log = open("/tmp/killed-%s.txt" % time.strftime("%Y%m%d"), "w")
+#kill_log = sys.stdout
 
 ps = subprocess.check_output(["ps", "-axo", "pid,rss,user,cmd", "-k-rss", "--no-headers"])
 
 kill_list = []
 
+i = 0
 for line in ps.split("\n"):
+    i += 1
+    try:
+        pid, rss, user, cmd = line.strip().split(None, 3)
+    except:
+        # ps would produce empty lines
+        print >>kill_log, 'Skip line %d: "%s"' % (i, line.strip())
+        continue
+
     pid, rss, user, cmd = line.strip().split(None, 3)
 
     if user in ["root"]:
@@ -22,9 +35,13 @@ for line in ps.split("\n"):
     if len(kill_list) == 10:
         break
 
-kill_log = open("/tmp/killed-%s.txt" % time.strftime("%Y%m%d"), "w")
 print >>kill_log, ["pid", "rss", "user", "cmd"]
 for row in kill_list:
     print >>kill_log, row
-    cmd = "kill -int %s" % (row[0])
+
+    #cmd = "kill -int %s" % (row[0])
+    # some processes like jupyter wouldn't quit on receiving SIGINT
+    cmd = "kill %s" % (row[0])
+    print >>kill_log, "    > " + cmd
+
     os.system(cmd)
